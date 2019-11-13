402c3625155e7aedf79e7515f53f9044e8afc815
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2019-11-13 21:16:43 +0900

    1차

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>하루 일정 관리 APP  by KH</h1>
+  </header>
+</template>
+
+
+<script>
+export default {
+
+
+}
+</script>
+
+<style scoped>
+  h1{
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
